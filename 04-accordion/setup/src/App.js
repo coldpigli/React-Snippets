5fede99d24ae0076c3cc8eb90e4cfe1bcb29ec7a
coldpigli <piyushdas98@gmail.com>
2021-08-16 21:24:50 +0530

    completed accordion in one go

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return <main>
+    <section className = "container">
+      <h3>Questions and Answers about Login</h3>
+      <section>
+      {
+          data.map((question)=>{
+            const {id,title,info} = question;
+            return <SingleQuestion key = {id} title = {title} info = {info}></SingleQuestion>
+          })
+        }
+      </section>
+        
+    </section>
+    
+  </main>
 }
 
 export default App;
