9fb51479184407e145d586626a91f868cc29e9f8
coldpigli <piyushdas98@gmail.com>
2021-08-17 01:15:01 +0530

    added dynamic buttons and filtering functionality

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -5,17 +5,28 @@ import items from './data';
 
 function App() {
 
-
-const [categories, setCategories] = useState([])
+const allCategories = ['all', ...new Set(items.map((item)=>item.category))] ; 
+console.log(allCategories);
+const [categories, setCategories] = useState(allCategories);
 const [foods, setFoods] = useState(items);
+
+const filterItem = (category) => {
+  if(category === 'all'){
+    setFoods(items);
+    return;
+  }
+  const newCategories=items.filter((item)=>item.category===category)
+  setFoods(newCategories);
+}
 
   return <main >
     <section className = "menu section">
     <div className="title">
       <h2>Piyush's Menu</h2>
       <div className="underline"></div>
+
     </div>
-    <Categories/>
+    <Categories categories={categories} filterItem = {filterItem}/>
     <Menu foods = {foods}/>
     </section>
     
