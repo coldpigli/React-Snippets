e5a3241e29b93f81afa29c8a9daeb82309a3245c
coldpigli <piyushdas98@gmail.com>
2021-08-17 13:29:45 +0530

    fetched data from API

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,7 +4,49 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [value, setValue] = useState(0);
+
+  const fetchData = async () => {
+        const response = await fetch(url);
+        const newJobs = await response.json();
+        setJobs(newJobs);
+        setIsLoading(false);
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  if(isLoading){
+    return <div className = "section laoding">
+          <h1>Loading...</h1>
+    </div> 
+    
+  } 
+
+  const{company,dates,duties,title} = jobs[value];
+  return <section className = "section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className = "underline"></div>
+      </div>
+      <article className = "job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className = 'job-date'>{dates}</p>
+        {duties.map((duty,index)=>{
+          return <div key={index} className = "job-desc">
+            <FaAngleDoubleRight/>
+            <p >
+              {duty}
+            </p>
+          </div>
+        })}
+      </article>
+  </section>
 }
 
 export default App
