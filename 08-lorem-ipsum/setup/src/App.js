a069156a98a12f2ba9d42ada777f9b368a211e74
coldpigli <piyushdas98@gmail.com>
2021-08-18 15:16:05 +0530

    completed the UI

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -2,9 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 function App() {
   
+
+
   
   return (
-  <h2>lorem ipsum project setup</h2>
+        <section className = "section-center"> 
+          <h3>Get modern day Lorem Ipsum</h3>
+          <form className = "lorem-form">
+              <label htmlFor="no_of_paragraphs">No Of Paragraphs</label>
+              <input type="number" name="no_of_paragraphs" id="no_of_paragraphs"></input>
+              <button type = "submit" className = "btn">Beer Me!</button>
+          </form>
+          <article className = "lorem-text">
+            <p>Hello this is a lorem ipsum generator </p>
+          </article>
+        </section>
     )
 }
 
