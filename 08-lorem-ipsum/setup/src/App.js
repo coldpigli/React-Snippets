7c630140f23e397d4e8c785952637dcc6f7ab613
coldpigli <piyushdas98@gmail.com>
2021-08-18 16:35:06 +0530

    added functionality, completed generator

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -3,18 +3,41 @@ import data from './data';
 function App() {
   
 
+const[paraCount, setParaCount] = useState(0);
 
-  
+const[paras, setParas] = useState([]);
+
+const handleOnChange = (e) => {
+      setParaCount(e.target.value);
+}
+
+const handleSubmit = (e) => {
+      e.preventDefault();
+      let amount = parseInt(paraCount);
+      setParas(data.slice(0,amount));
+}
+
+
   return (
         <section className = "section-center"> 
           <h3>Get modern day Lorem Ipsum</h3>
-          <form className = "lorem-form">
+          <form className = "lorem-form" onSubmit = {handleSubmit}>
               <label htmlFor="no_of_paragraphs">No Of Paragraphs</label>
-              <input type="number" name="no_of_paragraphs" id="no_of_paragraphs"></input>
+              <input type="number" 
+              name="no_of_paragraphs" 
+              id="no_of_paragraphs" 
+              value = {paraCount}
+              onChange = {handleOnChange}
+              min = '0'
+              max = '8'></input>
               <button type = "submit" className = "btn">Beer Me!</button>
           </form>
           <article className = "lorem-text">
-            <p>Hello this is a lorem ipsum generator </p>
+            {
+              paras.map((para,index)=>{
+                return <p key={index}>{para}</p>
+              })
+            }
           </article>
         </section>
     )
