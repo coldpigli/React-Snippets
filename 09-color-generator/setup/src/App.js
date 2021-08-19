d528486f4d6bf35a61fad1d5842c3dfbaa7a493c
coldpigli <piyushdas98@gmail.com>
2021-08-19 20:37:32 +0530

    UI completed/functionality pending

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,28 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+
+  const [inputText, setInputText] = useState("")
+  const [error, setError] = useState(false);
+  const [colorList, setColorList] = useState([]);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
+    return <>
+      <section  className = "container">
+        <h3>Color-Bomb</h3>
+        <form onSubmit = {handleOnSubmit}>
+          <input 
+          type = 'text' 
+          placeholder = "#fffff" 
+          value={inputText}
+          onChange = {(e)=>{setInputText(e.target.value)}}></input>
+          <button type = 'submit' className ="btn">Beer Me!</button>
+        </form>
+      </section>
+      <section className = "colors"></section>
+    </>
 }
 
 export default App
