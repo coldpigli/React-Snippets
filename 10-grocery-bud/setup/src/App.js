564f842383bd95d4bcae2539c73dd9a8920f9fe9
coldpigli <piyushdas98@gmail.com>
2021-08-20 23:51:22 +0530

    Intial UI completed

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,27 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+}
+
+  return <section className = "section-center">
+    <form className="grocery-form" onSubmit = {handleSubmit}>
+      <Alert/>
+      <h3>Grocery List</h3>
+      <div className = "form-control">
+        <input type="text"></input>
+        <button type ="submit" className = "submit-btn" placeholder = "e.g: Milk, Cheese">
+          {}
+        </button>
+      </div>
+    </form>
+    <div className = "grocery-container">
+      <List/>
+      <button className="clear-btn">Clear item</button>
+    </div>
+  </section>
 }
 
 export default App
